bee7372f27321ed31a679ad637d5d049993df74f
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2023-10-31 00:33:07 +0100

    updated configs for new gpu

--- .local/share/xinitrc.d/10-xrandr-monitors.sh
+++ .local/share/xinitrc.d/10-xrandr-monitors.sh
@@ -6,18 +6,18 @@ if [[ "$HOSTNAME" == "workstation" ]]; then
     RATE=${1:-120}
     DPI=96
 
-    xrandr --output DP-4 --auto
-    xrandr --output DP-2 --right-of DP-4 --auto
+    xrandr --output DisplayPort-0 --auto
+    xrandr --output DisplayPort-1 --right-of DisplayPort-0 --auto
 
-    xrandr --output DP-4 --mode $RES --rate "$RATE" --primary --auto
-    xrandr --output DP-2 --mode $RES --rate "$RATE" --right-of DP-4 --auto
+    xrandr --output DisplayPort-0 --mode $RES --rate "$RATE" --primary --auto
+    xrandr --output DisplayPort-1 --mode $RES --rate "$RATE" --right-of DisplayPort-0 --auto
 
     for monitor in $(xrandr | awk '/ connected.*panning/ {print $1}'); do
         xrandr --output "$monitor" --mode $RES --panning $RES --auto
         sleep 0.5
     done
 
-    xrandr --output DP-2 --right-of DP-4
+    xrandr --output DisplayPort-1 --right-of DisplayPort-0
     xrandr --dpi $DPI
 
 elif [[ "$HOSTNAME" == "laptop" ]]; then
